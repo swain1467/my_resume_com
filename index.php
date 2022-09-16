50a611cd29531a3db16b812d1a583f36e7d3a550
Subhendu Swain <56427833+swain1467@users.noreply.github.com>
2022-09-16 13:01:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body> 
+    <div class = "contact">
+        <div class = "container">
+            <div class = "about">
+                <h3>Subhendu Sekhar Swain</h3>
+                <h4>Email: Subhendu@gmail.com</h4>
+                <h4>Contact No: 8280885099</h4>
+            </div>
+            <div class = "image">
+                <img src="sss.jpg" alt="profile pic">
+            </div>
+        </div>
+    </div> 
+    <div class = "objective">
+            <div class = "container">
+                <h3>Career Objective</h3>
+                <p>Enhance my technical and analytical knowledge.Management of web on windows based
+                software applications. Design and develop ERP like HRMS(Human Resources Management System) and
+                MS(Inventory Management System).
+                </p>
+        </div>
+    </div> 
+    <div class = "exp">
+        <div class = "container">
+            <h3>Work With</h3>
+            <ul>
+                <li>Silicon Techlab Bhubaneswar</li> 
+                <h5>From: 14 Jan 2020</h5>   
+                <h5>To: 26 Aug 2022</h5>  
+                <h5>Software Engineer</h5>  
+            </ul> 
+        </div>
+    </div>
+    <div class = "skills">
+        <div class = "container">
+            <h3>Skills</h3>
+            <ul class = "skill-set">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>Java Script</li>
+                <li>Jquery</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>PostgresSql</li>
+                <li>MySql</li>
+            </ul>
+        </div>
+    </div>
+    <div class = "project">
+        <div class = "container">
+            <h3>Projects</h3>
+            <ul>
+                <li>apsdigicamps.com</li> 
+                    <h5>
+                        An erp for "Army public school". This project consist of different modules.
+                        I am responsible for maintenance and new enhancement of HRMS
+                        and Store module.
+                    </h5>
+                <li>erp.oav.edu.in</li> 
+                    <h5>
+                        An erp for "Army public school". This project consist of different modules.
+                        I am responsible for maintenance and new enhancement of HRMS
+                        and Store module.
+                    </h5>
+                <li>erp.stlindia.com</li> 
+                    <h5>
+                        An erp for "Silicon Techlab Pvt.ltd" I am maintaing the HRMS module
+                    </h5>
+            </ul> 
+        </div>
+    </div>
+    <div class = "education">
+        <div class = "container">   
+            <h3>Education</h3>
+            <ul class = "institute-list">
+                <li>CVRaman Global University Bhubaneswar</li>
+                    <h5>Betch</h5>
+                    <h5>8.38</h5>
+                    <h5>2022</h5>
+                <li>Vikram Deb Autonomous College Jeypore</li>
+                    <h5>12th</h5>
+                    <h5>74%</h5>
+                    <h5>2016</h5>
+                <li>Project Highschool Bariniput</li>
+                    <h5>10th</h5>
+                    <h5>78.5%</h5>
+                    <h5>2014</h5>
+            </ul>
+        </div>
+    </div>
+    <div class = "interests">
+        <div class = "container">
+            <h3>Interests</h3>
+            <ul>
+                <li>House Design in Floor Plan Creator App</li>
+                <li>raveling natural places</li>
+                <li>Bike riding</li>
+            </ul>
+        </div>
+    </div>
+    <div class = "declaration">
+        <div class = "container">
+            <p>I hereby confirm that all the details furnished above are authentic and accurate to the best of my belief.</p>
+            <div class = "place">Bhubaneswar</div>
+            <div class = "date">15-09-2022</div>
+        </div>
+    </div>
+</body>
+</html>
